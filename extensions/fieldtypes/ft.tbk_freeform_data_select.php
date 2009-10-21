6a3ed8f9c51c768ce31e64aea80e41127e6e306b
Tim Kelty <timkelty@gmail.com>
2009-10-21 09:59:31 -0400

    No more multi-selects. Just use a matrix for multiple.

--- extensions/fieldtypes/ft.tbk_freeform_data_select.php
+++ extensions/fieldtypes/ft.tbk_freeform_data_select.php
@@ -17,18 +17,12 @@ class Tbk_freeform_data_select extends Fieldframe_Fieldtype {
 		'no_lang'  => TRUE
 	);
   var $default_field_settings = array(
-  	'input_type' => 'select',
   	'data_type' => 'fields'
   );
 
   var $default_cell_settings = array(
-  	'input_type' => 'select',
   	'data_type' => 'fields'
   );
-  var $input_types = array(
-		'select'       => 'Drop-down List',
-		'multi'        => 'Multi-select List',
-	);
 	var $data_types = array(
 		'fields'       => 'Freeform Fields',
 		'templates'    => 'Freeform Templates',
@@ -58,12 +52,8 @@ class Tbk_freeform_data_select extends Fieldframe_Fieldtype {
   	$SD = new Fieldframe_SettingsDisplay();
 
   	return  '<label>'
-            . $DSP->qdiv('itemWrapper defaultBold', 'Data Type')
+            . $DSP->qdiv('itemWrapper defaultBold', 'Populate the select with:')
             . $SD->select('data_type', $cell_settings['data_type'], $this->data_types)
-        	  . '</label>'
-      	    .  '<label>'
-        	  . $DSP->qdiv('itemWrapperTop defaultBold', 'Input Type')
-        	  . $SD->select('input_type', $cell_settings['input_type'], $this->input_types)
         	  . '</label>'
         	  . '<div style="height: 5px;"></div>';
   }
@@ -79,7 +69,6 @@ class Tbk_freeform_data_select extends Fieldframe_Fieldtype {
   function display_field($field_name, $field_data, $field_settings)
   {
     global $DSP, $DB, $FF;
-    $field_name = $field_name . ($field_settings['input_type'] == 'multi' ? '[]' : '');
     
     $r = ($FF->row['field_required'] == 'y') ? '' : $DSP->input_select_option('', '--');
     
@@ -92,16 +81,11 @@ class Tbk_freeform_data_select extends Fieldframe_Fieldtype {
         $multi_row_count = 0;
         foreach($templates_q->result as $template)
         {
-          if (is_array($field_data)) {
-            $selected = in_array($template['template_name'], $field_data) ? 'y' : '';  
-          }
-          else {
-            $selected = ($template['template_name'] == $field_data) ? 'y' : '';  
-          }
+          $selected = ($template['template_name'] == $field_data) ? 'y' : '';  
           $r .= $DSP->input_select_option($template['template_name'], $template['template_label'], $selected);
           $multi_row_count++;
         }
-        $r = $DSP->input_select_header($field_name, ($field_settings['input_type'] == 'multi' ? 'y' : ''), ($multi_row_count < 15 ? $multi_row_count : 15), 'auto')
+        $r = $DSP->input_select_header($field_name, '', ($multi_row_count < 15 ? $multi_row_count : 15), 'auto')
         . $r
         . $DSP->input_select_footer();
       }
@@ -119,17 +103,12 @@ class Tbk_freeform_data_select extends Fieldframe_Fieldtype {
         $multi_row_count = 0;
         foreach($fields_q->result as $field)
         {
-          if (is_array($field_data)) {
-            $selected = in_array($field['name'], $field_data) ? 'y' : '';  
-          }
-          else {
-            $selected = ($field['name'] == $field_data) ? 'y' : '';  
-          }
+          $selected = ($field['name'] == $field_data) ? 'y' : '';  
           $r .= $DSP->input_select_option($field['name'], $field['label'], $selected);
           $multi_row_count++;
         }
 
-        $r = $DSP->input_select_header($field_name, ($field_settings['input_type'] == 'multi' ? 'y' : ''), ($multi_row_count < 15 ? $multi_row_count : 15), 'auto')
+        $r = $DSP->input_select_header($field_name, '', ($multi_row_count < 15 ? $multi_row_count : 15), 'auto')
             . $r
             . $DSP->input_select_footer();
       }
@@ -152,33 +131,5 @@ class Tbk_freeform_data_select extends Fieldframe_Fieldtype {
 	{
 		return $this->display_field($cell_name, $cell_data, $cell_settings);
 	}
-	
-	/**
-	 * Save Field
-	 * 
-	 * @param  mixed  $field_data      The field's current value
-	 * @param  array  $field_settings  The field's settings
-	 * @return mixed	Modified or same $field_data
-	 */
-  function save_field($field_data, $field_settings)
-  {
-   // remove empty data
-   if (is_array($field_data)) {
-    $field_data = array_filter($field_data);
-   }
-      
-   return $field_data;
-  }
-  /**
-	 * Save Cell
-	 *
-	 * @param  mixed   $cell_data      The cell's current value
-	 * @param  array   $cell_settings  The cell's settings
-	 * @return mixed   Modified $cell_data
-	 */
-	function save_cell($cell_data, $cell_settings)
-	{
-		return $this->save_field($cell_data, $cell_settings);
-	}
-	
+
 }
